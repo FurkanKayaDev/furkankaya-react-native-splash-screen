d775dda06081c0cdd685aa5161e131c6be1f0fc8
Furkan <furkan18388@gmail.com>
2024-02-08 15:38:26 +0300

    hotfix

--- ios/RNSplashScreen.h
+++ ios/RNSplashScreen.h
@@ -3,29 +3,11 @@
  * Author:Furkankayadev
  * GitHub:https://github.com/furkankayadev 
  */ 
-#import <React/RCTBridge.h>
+#import <React/RCTBridgeModule.h>
 #import <UIKit/UIKit.h>
 
-@interface RNSplashScreen : NSObject <RCTBridgeModule>
-
-RCT_EXPORT_MODULE(SplashScreen);
-
-/**
- * Shows a custom splash screen image in the given root view.
- *
- * @param screenName Name of the nib file containing the splash screen image.
- * @param rootView The root view where the splash screen should be displayed.
- */
-RCT_EXPORT_METHOD(showSplash:(NSString *)screenName rootView:(UIView *)rootView);
-
-/**
- * Shows the default splash screen.
- */
-RCT_EXPORT_METHOD(show);
-
-/**
- * Hides the splash screen.
- */
-RCT_EXPORT_METHOD(hide);
-
+@interface RNSplashScreen : NSObject<RCTBridgeModule>
++ (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView;
++ (void)show;
++ (void)hide;
 @end
